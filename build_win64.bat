f2119c4137e2ecd156a6a354f0e8f7698aa35609
Philippe Gagne <philippe.gagne@optelgroup.com>
2019-05-14 15:42:26 -0400

    Fix mexopencv recipe.

--- build_win64.bat
+++ build_win64.bat
@@ -1,45 +1,8 @@
-@echo off
+@echo on
 
-set MATLAB_ROOT_DIR="C:\Program Files\MATLAB\R2018b"
+set MATLAB_ROOT_DIR="C:\Program Files\MATLAB\R2019a"
 
-set CONDA_DEST_DEPOT_PATH=%cd%\..\conda
-call conda index %CONDA_DEST_DEPOT_PATH%
+call conda build %1 %2 %3 %4 %5 ^
+	--no-anaconda-upload ^
+	-c defaults
 if errorlevel 1 exit /b 1
-
-if "%1" neq "" (
-    call conda build %1 %2 %3 %4 %5 ^
-        --no-anaconda-upload ^
-        --output-folder %CONDA_DEST_DEPOT_PATH% ^
-        -c defaults
-    if errorlevel 1 exit /b 1
-) else (
-    call conda build mexopencv_recipe ^
-        --no-anaconda-upload ^
-        --output-folder %CONDA_DEST_DEPOT_PATH% ^
-        -c defaults
-    if errorlevel 1 exit /b 1
-
-    call conda build matgeom_recipe ^
-        --no-anaconda-upload ^
-        --output-folder %CONDA_DEST_DEPOT_PATH% ^
-        -c defaults
-    if errorlevel 1 exit /b 1
-
-    call conda build matlabfns_recipe ^
-        --no-anaconda-upload ^
-        --output-folder %CONDA_DEST_DEPOT_PATH% ^
-        -c defaults
-    if errorlevel 1 exit /b 1
-
-    call conda build pdollar_recipe ^
-        --no-anaconda-upload ^
-        --output-folder %CONDA_DEST_DEPOT_PATH% ^
-        -c defaults
-    if errorlevel 1 exit /b 1
-
-    call conda build vlfeat_recipe ^
-        --no-anaconda-upload ^
-        --output-folder %CONDA_DEST_DEPOT_PATH% ^
-        -c defaults
-    if errorlevel 1 exit /b 1
-)
